2c474b1ee8631b1eac51582aa26957bc25463416
steveyout <youtsteve1@gmail.com>
2024-08-16 16:55:34 +0300

    changed title

--- src/pages/admin/AdminPage.tsx
+++ src/pages/admin/AdminPage.tsx
@@ -12,7 +12,9 @@ export function AdminPage() {
     <SubPageLayout>
       <ThinContainer>
         <Heading1>Admin tools</Heading1>
-        <Paragraph>Silly tools used test StreamerFlix! ૮₍´˶• . • ⑅ ₎ა</Paragraph>
+        <Paragraph>
+          Silly tools used test StreamerFlix! ૮₍´˶• . • ⑅ ₎ა
+        </Paragraph>
 
         <ConfigValuesPart />
         <BackendTestPart />
